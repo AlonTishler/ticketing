cc66ca0e0297b353ba106bfa53926ae51a420c32
alon3185@gmail.com <alon3185@gmail.com>
2021-06-24 20:27:07 +0300

    added / after baseURL in build client

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -4,7 +4,7 @@ const buildClient = ({ req }) => {
   if (typeof window === 'undefined') {
     // we are on the server
     return axios.create({
-      baseURL: 'http://www.ticketingdotdev-app.xyz',
+      baseURL: 'http://www.ticketingdotdev-app.xyz/',
       headers: req.headers,
     });
   } else {
